e7c8a7d4c44ae1041b7fab60d40655687528d9f4
GiorgosDrog <cs04358@uoi.gr>
2024-05-07 02:16:42 +0300

    with no errors

--- BookStore_project/src/main/java/myy803/BookStore/entity/User.java
+++ BookStore_project/src/main/java/myy803/BookStore/entity/User.java
@@ -1,15 +1,29 @@
 package myy803.BookStore.entity;
 
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name="user")
 public class User {
-	private String username;
-	private String fullname;
-	private Role role;
 	
-	public User(String username, String fullname) {
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="role")
+	private String role;
+	
+	public User(String username, String password) {
 		this.username = username;
-		this.fullname = fullname;	
+		this.password = password;	
 	}
 	
 	public String getUsername() {
@@ -20,19 +34,19 @@ public class User {
 		this.username = username;
 	}
 
-	public String getFullname() {
-		return fullname;
+	public String getPassword() {
+		return password;
 	}
 
-	public void setFullname(String fullname) {
-		this.fullname = fullname;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
-	public Role getRole() {
+	public String getRole() {
 		return role;
 	}
 
-	public void setRole(Role role) {
+	public void setRole(String role) {
 		this.role = role;
 	}
 }
--- BookStore_project/target/classes/DDL/bookstore.sql
+++ BookStore_project/target/classes/DDL/bookstore.sql
@@ -1,80 +1,51 @@
-CREATE DATABASE IF NOT EXISTS `data_diplomas`;
-USE `data_diplomas`;
+CREATE DATABASE IF NOT EXISTS `bookstore`;
+USE `bookstore`;
 
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS `subject`;
-DROP TABLE IF EXISTS `application`;
-DROP TABLE IF EXISTS `professor`;
-DROP TABLE IF EXISTS `thesis`;
-DROP TABLE IF EXISTS `student`;
 DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `userprofile`;
+DROP TABLE IF EXISTS `book`;
+DROP TABLE IF EXISTS `bookcategory`;
+DROP TABLE IF EXISTS `bookauthor`;
 SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE `users` (
-  id int NOT NULL AUTO_INCREMENT,
-  role_id int NOT NULL,
-  user_name text DEFAULT NULL,
-  password text DEFAULT NULL,
-  role text DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  role varchar(255) DEFAULT NULL,
+  PRIMARY KEY (username,password)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `userprofile` (
+    userid int NOT NULL AUTO_INCREMENT,
+    user_fullname varchar(255) DEFAULT NULL,
+    user_name varchar(255) DEFAULT NULL, 
+    age int NOT NULL,
+    phonenumber int, 
+    PRIMARY KEY (userid)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
+CREATE TABLE `book` (
+    idbook int NOT NULL AUTO_INCREMENT,
+    title varchar(255) DEFAULT NULL,
+    userid int NOT NULL,
+    description text,
+    PRIMARY KEY (idbook),
+    FOREIGN KEY (userid) REFERENCES userprofile(userid)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-CREATE TABLE professor (
-  professor_id INT PRIMARY KEY AUTO_INCREMENT,
-  full_name VARCHAR(50) NOT NULL,
-  specialty VARCHAR(50)
-);
+CREATE TABLE `bookcategory` (
+    idcategory int NOT NULL AUTO_INCREMENT,
+    name varchar(255) DEFAULT NULL,
+    userid int NOT NULL,
+    PRIMARY KEY (idcategory),
+    FOREIGN KEY (userid) REFERENCES userprofile(userid)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-CREATE TABLE student (
-  student_id INT PRIMARY KEY AUTO_INCREMENT,
-  full_name VARCHAR(50) NOT NULL,
-  year_of_studies INT,
-  current_avg_grade DECIMAL(4,2),
-  remaining_courses_for_graduation INT
-);
-
-CREATE TABLE subject (
-  subject_id INT PRIMARY KEY AUTO_INCREMENT,
-  subject_name VARCHAR(500),
-  professor_id INT NOT NULL,
-  is_available boolean, 
-  description VARCHAR(500),
-  FOREIGN KEY (professor_id) REFERENCES professor(professor_id)
-);
-
-CREATE TABLE thesis (
-  thesis_id INT PRIMARY KEY AUTO_INCREMENT,
-  professor_id INT NOT NULL,
-  student_id INT,
-  subject_id INT NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  objectives VARCHAR(500),
-  supervisor VARCHAR(50),
-  implmentationgrade float,
-  reportgrade float,
-  presentationgrade float,
-  FOREIGN KEY (professor_id) REFERENCES professor(professor_id),
-  FOREIGN KEY (student_id) REFERENCES student(student_id),
-  FOREIGN KEY (subject_id) REFERENCES subject(subject_id)
-);
-
-CREATE TABLE application (
-  application_id INT PRIMARY KEY AUTO_INCREMENT,
-  student_id INT NOT NULL,
-  subject_id INT NOT NULL,
-  full_name VARCHAR(50) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  major VARCHAR(50) NOT NULL,
-  GPA DECIMAL(4,2) NOT NULL,
-  remaining_courses_for_graduation INT,
-  status VARCHAR(20) NOT NULL,
-  application_date DATE NOT NULL,
-  FOREIGN KEY (student_id) REFERENCES student(student_id),
-  FOREIGN KEY (subject_id) REFERENCES subject(subject_id)
-);
-
-
-
-
-
+CREATE TABLE `bookauthor` (
+    idauthor int NOT NULL AUTO_INCREMENT,
+    name varchar(255) DEFAULT NULL,
+    userid int NOT NULL,
+    PRIMARY KEY (idauthor),
+    FOREIGN KEY (userid) REFERENCES userprofile(userid)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
