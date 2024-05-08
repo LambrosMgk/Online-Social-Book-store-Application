c35b0fc6a0a15497c866e7ede0c8cbbfbd589435
LambrosMgk <116386147+LambrosMgk@users.noreply.github.com>
2024-05-08 23:27:39 +0300

    Added homepage part 2

--- BookStore_project/src/main/java/myy803/BookStore/service/UserServiceImpl.java
+++ BookStore_project/src/main/java/myy803/BookStore/service/UserServiceImpl.java
@@ -1,15 +1,34 @@
 package myy803.BookStore.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class UserServiceImpl implements UserDetailsService {
+import myy803.BookStore.entity.User;
+import myy803.BookStore.mapper.UserMapper;
+
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Override
+	public void saveUser(User user) {
+		String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userMapper.save(user);
+    }
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean isUserPresent(User user) {
+		User storedUser = userMapper.findByUsername(user.getUsername());
+		
+		if(storedUser == null)
+			return false;
+		
+		return true; //storedUser.method();
 	}
 
 }
