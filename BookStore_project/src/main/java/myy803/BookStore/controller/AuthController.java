859cc0f7320648d5ca9d32e1b0de17ff9f102c4e
LambrosMgk <116386147+LambrosMgk@users.noreply.github.com>
2024-05-09 21:21:44 +0300

    html update

--- BookStore_project/src/main/java/myy803/BookStore/controller/AuthController.java
+++ BookStore_project/src/main/java/myy803/BookStore/controller/AuthController.java
@@ -3,8 +3,11 @@ package myy803.BookStore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import myy803.BookStore.entity.User;
 import myy803.BookStore.service.UserService;
@@ -13,10 +16,23 @@ import myy803.BookStore.service.UserService;
 public class AuthController {
     @Autowired
     UserService userService;
-
-    @RequestMapping("/login")
-    public String login(){
-        return "auth/signin";
+    
+    @GetMapping("/")
+    public String homepage() {
+        return "homepage"; 		// Refers to a Thymeleaf or JSP file called "homepage"
+    }
+    
+    @PostMapping("/login")
+    public String handleLogin(@RequestParam String username, @RequestParam String password, Model model) {
+        // Example login check (should use proper security/authentication mechanisms)
+        if ("user".equals(username) && "pass".equals(password)) { // Simple example credentials
+            //return "redirect:/dashboard"; // Redirect to some authenticated area
+        } else {
+            model.addAttribute("error", true); // Show error if login fails
+            return "homepage"; // Redirect back to the homepage with an error
+        }
+        
+        return "homepage";
     }
 
     @RequestMapping("/register")
